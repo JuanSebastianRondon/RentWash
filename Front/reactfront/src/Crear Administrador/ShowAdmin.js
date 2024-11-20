6800365626508ae53fc8ca1908634b837dfc68ef
Juanse <megapikachu03@gmail.com>
2024-11-20 02:15:35 -0500

    actualizado

--- Front/reactfront/src/Crear Administrador/ShowAdmin.js
+++ Front/reactfront/src/Crear Administrador/ShowAdmin.js
@@ -5,6 +5,7 @@ import {link} from 'react-router-dom'
 const URI= 'http:://localhost:8000/Admin/'
 
 const CompShowAdmin=()=>{
+    
     const[Admin, setAdmin] =useState([])
     useEffect(()=>{
         getAdminById()
@@ -12,12 +13,21 @@ const CompShowAdmin=()=>{
 
     //Procedimineto para mostrar todos los admins
     const getAdmins =async()=>{
-
+        const res =await axios.get(URI);
+        setAdmin(res.data);
     }
 //procedimiento para eliminar un admin
     const deleteAdmin =async(id)=>{
-
+        axios.delete(`${URI}${id}`);
+        getAdmins();
     }
+    return(<div className='container'>
+            <div className='row'>
+                <div className='col'>
+                
+                </div>
+            </div>
+        </div>);
 
 }
 
--- node/SRC/config/dbConfig.js
+++ node/SRC/config/dbConfig.js
@@ -1,4 +1,3 @@
-//var mysql = require("mysql");
 import {Sequelize} from 'sequelize'
 
 const db = new Sequelize('rentalavadoras','Juanse','12345678',{
@@ -9,34 +8,3 @@ const db = new Sequelize('rentalavadoras','Juanse','12345678',{
 
 export default db;
 
-//const { Sequelize } = require('sequelize');
-
-/*
-const router =express.Router();
-
-router.get('/',function(req,res){
- res.send('Funciona')
-});
-
-module.exports=router;
-var conexion = mysql.createConnection({
-    host:"127.0.0.1",
-    database:"rentalavadoras",
-    user:"Juanse",
-    password:"12345678",
-    port:'3306',
-    authPlugins: {
-        mysql_native_password: () =>
-            require('mysql/lib/protocol/auth/mysql_native_password')
-    }
-});
-
-conexion.connect(function(err){
-    if(err){
-        throw err;
-    }
-    else{
-        console.log("Conectado a la base de datos");
-    }
-});
-*/
